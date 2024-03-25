a8e3d82271cab20f060d6fac1b7f941bf4d2e658
VINICIUS_GUARNIERI_SOUZA <22200460@OMEGAF20.FLORESTA>
2024-03-25 08:30:05 -0300

    primeiro commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+
+interface IForm {
+  opt: "logar" | "cadastrar" | "contato";
+}
+
+const Form = (opt: IForm) => {
+  if (opt.opt === "logar") {
+    return (
+      <>
+        <form id="formLogin">
+          <label>Email</label>
+          <input type="email" />
+          <label>Senha</label>
+          <input type="password" />
+          <button>Enviar</button>
+        </form>
+      </>
+    );
+  } else if (opt.opt === "cadastrar") {
+    return (
+      <>
+        <form id="formCadastro">
+          <label>Nome</label>
+          <button>Enviar</button>
+        </form>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <form id="formContato">
+        <label>Assunto</label>
+        <input type="text" />
+        <label>Nome</label>
+        <input type="text" />
+        <label>Mensagem</label>
+        <textarea></textarea>
+        <button>Enviar</button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
